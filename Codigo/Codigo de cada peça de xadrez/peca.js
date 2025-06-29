eda1b7a95792087604ec35c4eab02879b583b8a0
juanlucas36041 <juanlucas6309@gmail.com>
2025-06-29 15:58:20 -0300

    Update peca.js

--- Codigo/Codigo de cada peça de xadrez/peca.js
+++ Codigo/Codigo de cada peça de xadrez/peca.js
@@ -1,1 +1,20 @@
+const EMPTY = -1;
+const WHITE = 0;
+const BLACK = 1;
+
+
+class Peca
+{
+	constructor(tipo, posI, posJ, id){
+		this.tipo = tipo;
+		this.posI = posI;
+		this.posJ = posJ;
+		this.id = id;
+	}	
+}
+
+
+
+
+
 
